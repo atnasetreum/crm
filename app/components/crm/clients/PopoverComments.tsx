b8e622a7317d0e8c7ef3a40f91baa759774894f7
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2024-02-07 21:52:26 -0600

    feat: filter compaign type done

--- app/components/crm/clients/PopoverComments.tsx
+++ app/components/crm/clients/PopoverComments.tsx
@@ -1,26 +1,16 @@
 import { useState } from "react";
 
 import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
 import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
-import PersonAdd from "@mui/icons-material/PersonAdd";
-import Settings from "@mui/icons-material/Settings";
-import Logout from "@mui/icons-material/Logout";
 import FeedbackIcon from "@mui/icons-material/Feedback";
-import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
-import AddIcon from "@mui/icons-material/Add";
 
 import { stringToDateWithTime } from "@shared/utils";
 import { Client } from "@interfaces";
