dd48b352ed3e1874ab3650cf38f7a91715851fe4
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2024-02-14 08:41:37 -0600

    feat: test click notification

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,7 +7,7 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     scope: "/",
     start_url: "/",
-    name: "Customer Relationship Management",
+    name: "CRM",
     short_name: "CRM",
     description: "CRM Mario Gutierrez",
     icons: [
@@ -33,36 +33,4 @@ export default function manifest(): MetadataRoute.Manifest {
       },
     ],
   };
-  /*return {
-    theme_color: "#ffffff",
-    background_color: "#4a90e2",
-    display: "standalone",
-    scope: "/",
-    start_url: "/",
-    name: "CRM",
-    short_name: "crm",
-    description: "crm",
-    icons: [
-      {
-        src: "/icon-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/icon-256x256.png",
-        sizes: "256x256",
-        type: "image/png",
-      },
-      {
-        src: "/icon-384x384.png",
-        sizes: "384x384",
-        type: "image/png",
-      },
-      {
-        src: "/icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
-  };*/
 }
